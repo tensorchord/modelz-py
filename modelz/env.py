6fa84bac728aaffb6fec371389c5966822311344
Keming <kemingyang@tensorchord.ai>
2023-03-06 15:33:41 +0800

    feat: read host and api key from env (#10)
    * feat: read host and api key from env
    
    Signed-off-by: Keming <kemingyang@tensorchord.ai>
    
    * format
    
    Signed-off-by: Keming <kemingyang@tensorchord.ai>
    
    ---------
    
    Signed-off-by: Keming <kemingyang@tensorchord.ai>

---
+++ modelz/env.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+import os
+
+
+PREFIX: str = "MODELZ_"
+
+
+class EnvConfig:
+    def __init__(self) -> None:
+        self.api_key: str
+        self.host: str = "https://api.modelz.ai/"
+        self.update_from_env()
+
+    def update_from_env(self):
+        for key in ("api_key", "host"):
+            val = os.environ.get(f"{PREFIX}{key.upper()}")
+            setattr(self, key, val)
