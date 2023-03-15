af704ca4d46dcb3eb72eb4c3b3a53c4f5c53966c
Keming <kemingyang@tensorchord.ai>
2023-03-15 16:17:54 +0800

    feat: use semver in pdm (#12)
    Signed-off-by: Keming <kemingyang@tensorchord.ai>

--- test.py
+++ test.py
@@ -6,6 +6,9 @@ parser = ArgumentParser("modelz-test")
 parser.add_argument("--host", default="", help="API server host")
 parser.add_argument("--key", help="API key")
 parser.add_argument("--project", help="Project name")
+parser.add_argument(
+    "--serde", help="serilization method", choices=("json", "msgpack"), default="json"
+)
 
 
 if __name__ == "__main__":
@@ -14,8 +17,8 @@ if __name__ == "__main__":
         key=args.key,
         project=args.project,
         host=args.host if args.host else None,
+        serde=args.srede,
     )
     resp = client.inference(params={"time": 0})
     print(resp.data)
     print(client.metrics())
-
