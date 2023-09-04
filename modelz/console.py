9e25bc1eb30734affa08b33869ed8b88cc1acc6f
Keming <kemingyang@tensorchord.ai>
2023-09-05 01:32:27 +0800

    fix: support disable rich console (#38)
    feat: support disable rich console
    
    Signed-off-by: Keming <kemingyang@tensorchord.ai>

---
+++ modelz/console.py
@@ -0,0 +1,32 @@
+import re
+
+from rich.console import Console
+
+from modelz.env import EnvConfig
+
+config = EnvConfig()
+
+
+class ModelzConsole:
+    class Status:
+        def __init__(self, msg) -> None:
+            self.msg = msg
+
+        def __enter__(self):
+            print(self.msg)
+
+        def __exit__(self, exc_type, exc_value, traceback):
+            pass
+
+    def __init__(self) -> None:
+        self._remove_style = re.compile(r"\[.*?\]")
+        self.formatter = lambda msg: self._remove_style.sub("", msg)
+
+    def status(self, msg: str):
+        return self.Status(self.formatter(msg))
+
+    def print(self, msg: str):
+        print(self.formatter(msg))
+
+
+console = ModelzConsole() if config.disable_rich else Console()
